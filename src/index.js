29c50824c1022f12de0e5c10740c5da7353be2aa
kayson <kaysonf.developer@gmail.com>
2022-02-19 18:34:53 +0800

    assets

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import _ from 'lodash';
+import './style.css';
+import CatPic from './cat.jpg';
+import Data from './data.xml';
+import Notes from './data.csv';
+import yaml from './data.yaml';
+
+console.log(yaml.title); // output `YAML Example`
+console.log(yaml.owner.name); // output `Tom Preston-Werner`
 
 function component() {
     const element = document.createElement('div');
 
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+    // example loading style
+    element.classList.add('hello');
+
+    // example loading image
+    const catIcon = new Image();
+    catIcon.src = CatPic;
+
+    element.appendChild(catIcon);
+
+    // example loading data
+    console.log(Data);
+    console.log(Notes)
 
     return element;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,46 @@
 const path = require('path');
+const yaml = require('yamljs');
 
 module.exports = {
+
     entry: './src/index.js',
+
     output: {
-        filename: 'main.js',
+        filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+
+            //https://webpack.js.org/guides/asset-management/#loading-images
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource', //https://webpack.js.org/guides/asset-modules/
+            },
+
+            //https://webpack.js.org/guides/asset-management/#loading-data
+            {
+                test: /\.(csv|tsv)$/i,
+                use: ['csv-loader'],
+            },
+            {
+                test: /\.xml$/i,
+                use: ['xml-loader'],
+            },
+
+            //https://webpack.js.org/guides/asset-management/#customize-parser-of-json-modules
+            {
+                test: /\.yaml$/i,
+                type: 'json',
+                parser: {
+                    parse: yaml.parse,
+                },
+            }
+        ],
     },
 };
