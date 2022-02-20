e37ae8ba233b05bed984f517b771966f44f54b14
kayson <kaysonf.developer@gmail.com>
2022-02-20 13:58:29 +0800

    code split

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
-import _ from 'lodash';
-import './style.css';
 
-import printMe from './print.js';
+async function getComponent() {
 
-
-function component() {
     const element = document.createElement('div');
 
-    // Lodash, currently included via a script, is required for this line to work
+    const { default: _ } = await import('lodash'); // could import a different library based on a computed variable
+
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-    // example loading style
-    element.classList.add('hello');
-
-    // output management example
-    const btn = document.createElement('button');
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
-
-    element.appendChild(btn)
 
     return element;
 }
 
-document.body.appendChild(component());
+getComponent().then((component) => {
+    document.body.appendChild(component);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -5,22 +5,18 @@ module.exports = {
 
     mode: 'development',
 
-    devtool: 'inline-source-map',
-
     devServer: {
         static: './dist',
     },
 
     entry: {
         index: './src/index.js',
-        print: './src/print.js',
     },
 
     output: {
         filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
         clean: true,
-        publicPath: '/',
     },
 
     plugins: [
